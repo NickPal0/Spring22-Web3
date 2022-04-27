0b30af93ed39412a782a559ee5dc7e0bcf1c53a2
NickPal0 <nick22palomino@gmail.com>
2022-04-26 22:39:26 -0700

    week 9
    added what we talked about in class

---
+++ Week_9/java/script.js
@@ -0,0 +1,8 @@
+document.getElementById("cat1").addEventListener("click", (event)=> 
+{event.target.closest("#cat1").classList.toggle("active")})
+
+document.getElementById("cat4").addEventListener("click", (event)=> 
+{event.target.closest("#cat4").classList.toggle("active")})
+
+document.getElementById("cat6").addEventListener("click", (event)=> 
+{event.target.closest("#cat6").classList.toggle("active")})
