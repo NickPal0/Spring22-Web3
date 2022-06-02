09c6129a5ee9d92434ecfd3e90981cb900aae7f4
NickPal0 <nick22palomino@gmail.com>
2022-06-02 16:51:27 -0700

    final update

--- final/sketch.js
+++ final/sketch.js
@@ -1,26 +1,23 @@
 
-let pos = 25;
-
-const faders = document.querySelector('.fade-in')
-console.info(faders[0])
-const appearOptions = { };
+const faders = document.querySelectorAll('.fade-in')
+console.log(faders)
+const appearOptions = { }
 
 const appearOnScroll = new IntersectionObserver
-(function(
-    entries,
-     appearOnScroll
-     ) {
-         entries.forEach(entry =>{
-             console.info(entry)
-             if (!entry.isIntersecting){
-                 return;
-             } else {
-                 entry.target.classList.add('appear');
-                 appearOnScroll.unobserve(entry.target);
-             }
-         })
-     }, 
-     appearOptions)
+    (
+        function(entries,appearOnScroll) 
+            {
+                 entries.forEach(entry => {
+                 if (!entry.isIntersecting){
+                     return;
+                } else {
+                         entry.target.classList.add('appear');
+                         appearOnScroll.unobserve(entry.target);
+                }
+              })
+             }, 
+     appearOptions
+    )
 
 
      faders.forEach(fader => {
